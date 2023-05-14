466619d26a8d012a90b48c2729a6090afb1ca422
Juan <juan.parra@smartsoft.com.co>
2023-05-14 17:26:48 -0500

    Interfaz de menu , Resumen y Datos con total a pagar de la caferia

---
+++ src/context/QuioscoProvider.jsx
@@ -0,0 +1,113 @@
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/router'
+
+const QuioscoContext = createContext()
+const QuioscoProvider = ({ children }) => {
+    const [categorias, setCategorias] = useState([])
+    const [categoriaActual, setCategoriaActual] = useState({})
+    const [producto, setProducto] = useState({})
+    const [modal, setModal] = useState(false)
+    const [pedido, setPedido] = useState([])
+    const [nombre, setNombre] = useState('')
+    const [total, setTotal] = useState(0)
+
+    const router = useRouter()
+
+
+    // Funtions y useEffect
+    const obtenerCategorias = async () => {
+        const { data } = await axios('/api/categorias')
+        setCategorias(data)
+    }
+    useEffect(() => {
+        obtenerCategorias()
+    }, [])
+
+    useEffect(() => {
+        setCategoriaActual(categorias[0])
+    }, [categorias])
+
+    useEffect(() => {
+        const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
+        setTotal(nuevoTotal)
+    }, [pedido])
+
+    const handleClickCategoria = id => {
+        const categoria = categorias.filter(cat => cat.id === id)
+        setCategoriaActual(categoria[0])
+        router.push('/')
+    }
+
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
+
+    const handleChangeModal = () => {
+        setModal(!modal)
+    }
+
+    const handleAgregarPedido = ({ categoriaId, ...producto }) => {
+        if (pedido.some(productoState => productoState.id === producto.id)) {
+            // Actualizar la cantidad
+            const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState)
+            setPedido(pedidoActualizado)
+
+            toast.success('Guardado correctamente')
+        } else {
+            setPedido([...pedido, producto])
+            toast.success('Agregado al Pedido')
+        }
+
+        setModal(false)
+    }
+
+    const handleEditarCantidades = id => {
+        const productoActualizar = pedido.filter(producto => producto.id === id)
+        setProducto(productoActualizar[0])
+
+        setModal(!modal)
+    }
+
+    const handleEliminarProducto = id => {
+        const pedidoActualizado = pedido.filter(producto => producto.id !== id)
+        setPedido(pedidoActualizado)
+    }
+
+    const colocarOrden = async (e) => {
+        e.preventDefault();
+        console.log(pedido)
+        console.log(nombre)
+        console.log(total)
+    }
+
+    //Return 
+    return (
+        <QuioscoContext.Provider
+            value={{
+                categorias,
+                categoriaActual,
+                handleClickCategoria,
+                producto,
+                handleSetProducto,
+                modal,
+                handleChangeModal,
+                handleAgregarPedido,
+                pedido,
+                handleEditarCantidades,
+                handleEliminarProducto,
+                nombre,
+                setNombre,
+                colocarOrden,
+                total
+
+            }}>
+            {children}
+        </QuioscoContext.Provider>
+    )
+}
+export {
+    QuioscoProvider
+}
+export default QuioscoContext
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Layout from "@/layout/Layout"
+import useQuiosco from "@/hooks/useQuiosco"
+import Producto from "@/components/Producto"
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const { categoriaActual } = useQuiosco()
+
   return (
-    <h1>Next.js</h1>
+    <Layout pagina={`Menu ${categoriaActual?.nombre}`}>
+      <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
+      <p className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuacion
+      </p>
+      <div
+        className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-4"
+      >
+        {categoriaActual?.Productos?.map(producto => (
+          < Producto key={producto.id} producto={producto} />
+        ))}
+      </div>
+    </Layout>
+
+
   )
 }
+
+
